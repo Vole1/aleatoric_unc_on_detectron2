7eefa5c34cfee0a6494c6573652a4c5060e2b3db
Aleksej Lapenok <live-penguin@yandex-team.ru>
2021-04-06 11:39:08 +0300

    use adam for optimize

--- detectron2/solver/build.py
+++ detectron2/solver/build.py
@@ -121,11 +121,11 @@ def build_optimizer(cfg: CfgNode, model: torch.nn.Module) -> torch.optim.Optimiz
         bias_lr_factor=cfg.SOLVER.BIAS_LR_FACTOR,
         weight_decay_bias=cfg.SOLVER.WEIGHT_DECAY_BIAS,
     )
-    return maybe_add_gradient_clipping(cfg, torch.optim.SGD)(
+    return maybe_add_gradient_clipping(cfg, torch.optim.Adam)(
         params,
         lr=cfg.SOLVER.BASE_LR,
-        momentum=cfg.SOLVER.MOMENTUM,
-        nesterov=cfg.SOLVER.NESTEROV,
+        # momentum=cfg.SOLVER.MOMENTUM,
+        # nesterov=cfg.SOLVER.NESTEROV,
         weight_decay=cfg.SOLVER.WEIGHT_DECAY,
     )
 
